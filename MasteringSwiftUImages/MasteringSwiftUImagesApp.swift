d978d8f9f1c5c13c09677b448fd4660f23a194a2
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-14 18:20:19 -0700

    Initial Commit

---
+++ MasteringSwiftUImages/MasteringSwiftUImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteringSwiftUImagesApp.swift
+//  MasteringSwiftUImages
+//
+//  Created by Guy Steinberg on 2024-04-14.
+//
+
+import SwiftUI
+
+@main
+struct MasteringSwiftUImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
